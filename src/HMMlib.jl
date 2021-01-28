df040a91969f07cc75cb72482f585d5fe03575e4
Yujia Liu <yl759@cam.ac.uk>
2021-01-28 00:18:36 +0000

    Mixture Gaussian model, modified both Baum-Welch

--- src/HMMlib.jl
+++ src/HMMlib.jl
@@ -49,29 +49,45 @@ end
 
 # Definition of HMM with continuous emission distribution
 struct Gaussian_HMM
+    # Gaussian mixture model
     hidden_state_space::Array{<:Any, 1}
     initial_distribution::Array{Float64, 1}
     transition_matrix::Array{Float64, 2}
-    Gaussian_center::Array{Float64, 1}
-    Gaussian_deviation::Array{Float64, 1}
+    Gaussian_center::Array{Float64, 2}
+    Gaussian_deviation::Array{Float64, 2}
+    mixture_coefficient::Array{Float64, 2}
     name::String
     # check parameter dimensionalities
-    function Gaussian_HMM(S, μ⁰, A, m, σ², name = "HMM_model")
+    function Gaussian_HMM(S, μ⁰, A, M, σ², W, name = "HMM_model")
         J = length(S)    # number of hidden states
+        K = size(W, 2)    # number of mixtures
         if length(μ⁰) != J
-            error("Size of hidden state space: $J and 
-            size of initial distribution: $(length(μ⁰)) mismatch")
+            error("Size of hidden state space: $J and " *
+            "size of initial distribution: $(length(μ⁰)) mismatch")
         elseif size(A) != (J, J)
-            error("Size of hidden state space: $J disagrees with 
-            size of transition matrix: $(size(A, 1)) × $(size(A, 2))")
+            error("Size of hidden state space: $J disagrees with " *
+            "size of transition matrix: $(size(A, 1)) × $(size(A, 2))")
         elseif !all(x -> abs(x - 1.0) < 1e-12, sum(A, dims = 2))
             error("The transition matrix is not normalized")
         elseif abs(sum(μ⁰) - 1.0) ≥ 1e-12
             error("The initial distribution is not normalized")
+        elseif size(M) != (J, K)
+            error("The size of Gaussian center matrix: " *
+            "$(size(M, 1)) × $(size(M, 2)) is incorrect " *
+            "Should be $J × $K")
+        elseif size(σ²) != (J, K)
+            error("The size of Gaussian deviation matrix: " *
+            "$(size(σ², 1)) × $(size(σ², 2)) is incorrect " *
+            "Should be $J × $K")
+        elseif size(W, 1) != J
+            error("The size of mixture coefficient $(size(W, 1)) × $(size(W, 2)) " *
+            "does not agree with the size of hidden state space $J")
+        elseif !all(x -> abs(x - 1.0) < 1e-12, sum(W, dims = 2))
+            error("The Gaussian mixture coefficient is not normalized")
         end
         # check Gaussian distribution parameters
         # TODO: currently not determined whether to use mixed Gaussian
-        new(S, μ⁰, A, m, σ², name)
+        new(S, μ⁰, A, M, σ², W, name)
     end
 end
 
@@ -95,7 +111,8 @@ function get_param(model::Gaussian_HMM)
         model.initial_distribution,
         model.transition_matrix,
         model.Gaussian_center,
-        model.Gaussian_deviation
+        model.Gaussian_deviation,
+        model.mixture_coefficient
     )
 end
 
@@ -115,8 +132,8 @@ function get_config(model::Gaussian_HMM)
 
     return (
         model.hidden_state_space,
-        length(model.hidden_state_space)
-        # TODO: if using mixture distribution, also return number of categories
+        length(model.hidden_state_space),
+        size(model.mixture_coefficient, 2)
     )
 end
 
@@ -145,7 +162,7 @@ end
 function emit(model::Gaussian_HMM, l::Int64)
     # Generate observed sequence - continuous version
 
-    μ⁰, A, m, σ² = get_param(model)
+    μ⁰, A, M, σ², W = get_param(model)
     S, J = get_config(model)
     # Hidden state sequence
     X_idx = zeros(Int64, l)    # allocate mem
@@ -155,29 +172,31 @@ function emit(model::Gaussian_HMM, l::Int64)
     end
     X = S[X_idx]    # convert to hidden state space
     # Observed sequence
-    d = [ Normal(m[i], sqrt(σ²[i])) for i in 1:J ]
+    d = [ MixtureModel(Normal, collect(zip(M[i, :], sqrt.(σ²[i, :]))), W[i, :]) 
+    for i in 1:J ]
     Y = [ rand(d[i]) for i in X_idx ]
     return X, Y
 end
 
-function b(model::AbstractHMM, j::Int64, v, ϵ = 0.01)
+function b(model::HMM, j::Int64, v, ϵ = 0.01)
     # The probability that v is observed given hidden state jl
     # bⱼ(v) = P(Yₙ = v|Xₙ = sⱼ)
 
-    if typeof(model) == HMM
-        # Discrete HMM
-        B = model.emission_matrix
-        V = model.emission_space
-        v_idx = findfirst(V .== v)    # get the index of v
-        return B[j, v_idx]
-    elseif typeof(model) ==  Gaussian_HMM
-        # Continuous observation HMM
-        m = model.Gaussian_center
-        σ² = model.Gaussian_deviation
-        d = Normal(m[j], sqrt(σ²[j]))
-        # take the integral around a small vicinity
-        return cdf(d, v + ϵ) - cdf(d, v - ϵ)
-    end
+    B = model.emission_matrix
+    V = model.emission_space
+    v_idx = findfirst(V .== v)    # get the index of v
+    return B[j, v_idx]
+end
+
+function b(model::Gaussian_HMM, j::Int64, v, ϵ = 0.01)
+    # The continuous version
+
+    M = model.Gaussian_center
+    σ² = model.Gaussian_deviation
+    W = model.mixture_coefficient
+    d = MixtureModel(Normal, collect(zip(M[j, :], sqrt.(σ²[j, :]))), W[j, :])
+    # take the integral around a small vicinity
+    return cdf(d, v + ϵ) - cdf(d, v - ϵ)
 end
 
 function forward(model::AbstractHMM, Y::AbstractArray)
@@ -248,39 +267,42 @@ function baum_welch(initial_model::HMM, Y::AbstractArray, threshold = 1e-2)
     N = length(Y)    # size of observed sequence
     # convert state to index
     V_idx = Dict( V[i] => i for i in 1:K )
+
     # the parameters are already initialized inside initial_model
     model = initial_model   # copy the initial model
     Δ = Inf    # difference between the current and previous log-likelihood
     lnL = log_likelihood(model, Y)    # initial log likelihood
+
     while(Δ > threshold)
         # variable shorthands
         μ⁰, A, B = get_param(model)
         # the forward and backward matrix
         β̂, α̂ , C = backward(model, Y)
 
-        # update the transition matrix
-        # the expcted count of i -> j transition
-        En = zeros(Float64, (J, J))
-        for i in 1:J
-            for j in 1:J
-                En[i, j] = A[i, j] * α̂[i, 1:(N - 1)] ⋅ 
-                (B[j, [ V_idx[Y[k]] for k in 2:N ]] .* β̂[j, 2:N])
-            end
+        # γₙ(i) is the possibility of the n_th hidden value being sᵢ
+        # γₙ(i) = P(Xₙ = sᵢ|Y;θ)
+        γ = C' .* α̂ .* β̂
+        # ξₙ(i, j) is the possibility that the n_th hidden state is sᵢ
+        # and transit to n+1_th hidden state of sⱼ
+        # ξₙ(i, j) = P(Xₙ = sᵢ, Xₙ₊₁ = sⱼ|Y; θ)
+        ξ = zeros(Float64, (J, J, N - 1))    # allocate mem
+        for n  = 1:(N - 1)
+            ξ[:, :, n] = α̂[:, n] * (B[:, V_idx[Y[n + 1]]] .* β̂[:, n + 1])' .* A
         end
-        A_new = En ./ (sum(En, dims = 2) * ones(Float64, (1, J)))    # normalize
 
+        # update the transition matrix
+        A_new = sum(ξ, dims = 3) ./ sum(γ[:, 1:(N - 1)], dims = 2)
+        A_new = dropdims(A_new, dims = 3)
         # update the initial distribution
-        μ⁰_new = α̂[:, 1] .* (A * (B[:, V_idx[Y[2]]] .* β̂[:, 2]))
-
+        μ⁰_new = γ[:, 1]
         # update the emission matrix
-        # define γₙ(i) = P(Xₙ = sᵢ|Y;θ)
-        γ = (ones(Float64, J) * reshape(C, (1, N))) .* α̂ .* β̂
         B_new = zeros(Float64, (J, K))    # allocate mem
         for i in 1:K
             # manually add a false: the Nth col will not be accessed
-            B_new[:, i] = sum(γ[:, [Y[1:(N-1)] .== V[i]; false]], dims = 2)
+            #B_new[:, i] = sum(γ[:, [Y[1:(N-1)] .== V[i]; false]], dims = 2)
+            B_new[:, i] = sum(γ[:, Y .== V[i]], dims = 2)
         end
-        B_new ./= sum(B_new, dims = 2) * ones(Float64, (1, K))
+        B_new ./= sum(B_new, dims = 2)
 
         # update the HMM model
         model = HMM(S, V, μ⁰_new, A_new, B_new, initial_model.name)
@@ -297,7 +319,7 @@ function baum_welch(initial_model::Gaussian_HMM, Y::AbstractArray, threshold = 1
     # Continuous version of Baum-Welch
 
     # shorthands of variables
-    S, J = get_config(initial_model)
+    S, J, K = get_config(initial_model)
     N = length(Y)    # size of observed sequence
     # the parameters are already initialized inside initial_model
     model = initial_model   # copy the initial model
@@ -305,40 +327,61 @@ function baum_welch(initial_model::Gaussian_HMM, Y::AbstractArray, threshold = 1
     lnL = log_likelihood(model, Y)    # initial log likelihood
     while(Δ > threshold)
         # variable shorthands
-        μ⁰, A, m, σ² = get_param(model)
+        μ⁰, A, M, σ², W = get_param(model)
         # the forward and backward matrix
         β̂, α̂ , C = backward(model, Y)
 
-        # update the transition matrix
-        # the expcted count of i -> j transition
-        En = zeros(Float64, (J, J))
-        for i in 1:J
-            for j in 1:J
-                En[i, j] = A[i, j] * α̂[i, 1:(N - 1)] ⋅ 
-                ([ b(model, j, Y[n]) for n in 2:N ] .* β̂[j, 2:N])
+        # γₙ(i) is the possibility of the n_th hidden value being sᵢ
+        # ξₙ(i, j) is the possibility that the n_th hidden state is sᵢ
+        # and transit to n+1_th hidden state of sⱼ
+        γ = zeros(Float64, (J, K, N))    # allocate mem
+        for n = 1:N
+            p = C[n] * α̂[:, n] .* β̂[:, n]
+            w = zeros(Float64, (J, K))
+            for j = 1:J
+                for k = 1:K
+                    w[j, k] = W[j, k] * pdf(Normal(M[j, k], sqrt(σ²[j, k])), Y[n])
+                end
             end
+            w ./= sum(w, dims = 2)
+            γ[:, :, n] = p .* w
         end
-        A_new = En ./ (sum(En, dims = 2) * ones(Float64, (1, J)))    # normalize
-        
-        # define γₙ(i) = P(Xₙ = sᵢ|Y;θ)
-        # the probability that n^th state being sᵢ, given observed seq
-        γ = (ones(Float64, J) * reshape(C, (1, N))) .* α̂ .* β̂
+        ξ = zeros(Float64, (J, J, N - 1))    # allocate mem
+        for n  = 1:(N - 1)
+            ξ[:, :, n] = α̂[:, n] * ([ b(model, j, Y[n + 1]) for j in 1:J ] .* β̂[:, n + 1])' .* A
+        end
+
+        # update the transition matrix
+        A_new = sum(ξ, dims = 3) ./ sum(γ[:, :, 1:(N - 1)], dims = (2, 3))
+        A_new = dropdims(A_new, dims = 3)    # remove singleton dimension
 
         # update the initial distribution
-        μ⁰_new = γ[:, 1]
+        μ⁰_new = sum(γ[:, :, 1], dims = 2)
+        μ⁰_new = vec(μ⁰_new)
+
+        # update the mixture coefficient
+        W_new = sum(γ, dims = 3)
+        W_new = dropdims(W_new, dims = 3)
+        W_new ./= sum(W_new, dims = 2)
 
         # update the mean of Gaussian distribution
-        m_new = (γ * Y) ./ sum(γ, dims = 2)
-        m_new = vec(m_new)    # reshape to a column vector
+        M_new = zeros(Float64, (J, K))
+        for k in 1:K
+            M_new[:, k] = (γ[:, k, :] * Y) ./ sum(γ[:, k, :], dims = 2)
+        end
 
         # update the deviation of Gaussian
-        σ²_new = zeros(Float64, J)
+        σ²_new = zeros(Float64, (J, K))
         for i = 1:J
-            σ²_new[i] = sum(((Y .- m[i]).^2 .* γ[i, :])[1:(N - 1)]) / sum(γ[i, 1:(N - 1)])
+            for k in 1:K
+                # σ²_new[i, k] = sum(((Y .- M[i, k]).^2 .* γ[i, k, :])[1:(N - 1)]) / 
+                # sum(γ[i, k, 1:(N - 1)])
+                σ²_new[i, k] = sum((Y .- M[i, k]).^2 .* γ[i, k, :]) / sum(γ[i, k, :])
+            end
         end
 
         # update the HMM model
-        model = Gaussian_HMM(S, μ⁰_new, A_new, m_new, σ²_new, initial_model.name)
+        model = Gaussian_HMM(S, μ⁰_new, A_new, M_new, σ²_new, W_new, initial_model.name)
 
         # update Δln(L)
         new_lnL = log_likelihood(model, Y)
